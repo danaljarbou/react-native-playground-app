47a7fbd1f76d9b528fe3d1cfa929153c4791916d
Dana <danaljarbou@gmail.com>
2021-12-13 10:05:06 +0300

    Login

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { SafeAreaViewBase, StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
+
+export default function App(){
+
+  return( 
+    <LoginScreen />
+
+  );
+  
+  }
